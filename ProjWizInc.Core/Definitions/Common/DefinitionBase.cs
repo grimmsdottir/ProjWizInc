4489f595f1a9f6d624c8e491497f3d6ba692cb13
grimmsdottir <grimmsdottir93@gmail.com>
2026-07-13 19:58:11 +0800

    working on the ECS refactor

--- ProjWizInc.Core/Definitions/Common/DefinitionBase.cs
+++ ProjWizInc.Core/Definitions/Common/DefinitionBase.cs
@@ -5,9 +5,5 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjWizInc.Core.Definitions.Common {
-    public class DefinitionBase {
-        public string Key { get; protected set; } = string.Empty;
-        public string DisplayName { get; protected set; } = string.Empty;
-        public int Id { get; protected set; } = -1;
-    }
+    
 }
--- ProjWizInc.Core/Definitions/Definitions.cs
+++ ProjWizInc.Core/Definitions/Definitions.cs
@@ -6,16 +6,5 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjWizInc.Core.Definitions {
-    public class ResourceDefinition : DefinitionBase{
-        public ResourceDefinition() {
-        }
-    }
-    public class JobDefinition : DefinitionBase {
-        public List<FeatureInterface> Features { get; set; } = new();
-
-        // Helper method to quickly find a specific component
-        public T GetComponent<T>() where T : FeatureInterface {
-            return Features.OfType<T>().FirstOrDefault();
-        }
-    }
+    
 }
